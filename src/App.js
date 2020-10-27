28a2632ea097a18e7b2dd250fb6eed97f8f57e9f
samson-corbiere <samson.corbiere@gmail.com>
2020-10-27 10:17:03 +0100

    simpsons react07 finish

---
+++ src/App.js
@@ -0,0 +1,113 @@
+/* import './App.css';
+import DisplayEmployee from './components/DisplayEmployee'
+import React from 'react'
+import axios from 'axios';
+
+const sampleEmployee = {
+  gender: 'male',
+  name: {
+    first: 'Charlie',
+    last: 'Thompson',
+  },
+  location: {
+    street: {
+      number: 761,
+      name: 'Tay Street',
+    },
+    city: 'Timaru',
+    postcode: 76111,
+  },
+  email: 'charlie.thompson@example.com',
+  picture: {
+    medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
+  },
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employee: sampleEmployee
+    };
+    this.getEmployee = this.getEmployee.bind(this);
+  }
+
+  getEmployee() {
+    // Send the request
+    axios.get('https://randomuser.me/api?nat=fr')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        this.setState({
+          employee: data.results[0],
+        });
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <DisplayEmployee employee={this.state.employee} />
+        <button type="button" onClick={this.getEmployee}>Get employee</button>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
+ */
+
+import './App.css';
+import DisplayQuote from './components/DisplayQuote'
+import React from 'react'
+import axios from 'axios';
+
+const sampleQuote = {
+    quote: "By chilling my loins I increase the chances of impregnating my wife.",
+    character: "Apu Nahasapeemapetilon",
+    image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+    characterDirection: "Left"
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: sampleQuote
+    };
+    this.getQuote = this.getQuote.bind(this);
+  }
+
+  getQuote() {
+    // Send the request
+    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      
+      // Use this data to update the state
+      .then(data => {
+        console.log(data)
+        this.setState({
+          quote: data[0],
+        });
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <DisplayQuote quote={this.state.quote} />
+        <button type="button" onClick={this.getQuote}>Get quote</button>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
